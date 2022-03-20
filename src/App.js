f732753f569eafd0dfce12699a148f93d14fde58
Ben Guericke <ben.guericke@gmail.com>
2022-03-19 19:52:58 -0600

    Changed from withAuthenticator to Authenticator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import {Authenticator} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
-  return (
-      <div className="App">
-        <header>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>We now have Auth!</h1>
-        </header>
-      </div>
-  );
+    return (
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
+    );
 }
 
-export default withAuthenticator(App);
+export default App;
