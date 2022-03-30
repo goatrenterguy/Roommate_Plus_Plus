b1d14b8345cff1cc4f8a3ad37376d938a025e8c2
benguericke <ben.guericke@gmail.com>
2022-03-30 13:00:35 -0600

    Changed source of links for nav items so that the same list can be used in multiple navs

---
+++ src/Components/DataObjects/getNavLinks.js
@@ -0,0 +1,8 @@
+function getNavLinks() {
+    return [
+        {text: "Page 1", href: "/page_1"},
+        {text: "Page 2", href: "/page_2"}
+    ];
+}
+
+export default getNavLinks();
--- src/Components/TopNav/NavLinkBar.js
+++ src/Components/TopNav/NavLinkBar.js
@@ -1,12 +1,31 @@
 import NavLink from './NavLink'
+import React from 'react'
+import getNavLinks from "../DataObjects/getNavLinks";
+class NavLinkBar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            'links': getNavLinks
+        };
+        this.getLinks = this.getLinks.bind(this);
+    }
 
-function NavLinkBar(props) {
-    return (
-        <ul className="navbar-nav">
-            <NavLink text="Link 1" href="page_1"/>
-            <NavLink text="Mina Smells Bad" href="page_2"/>
-        </ul>
-    );
+    getLinks() {
+        let navLinkDoms = [];
+        function getLinkDoms(value) {
+            navLinkDoms.push(<NavLink text={value.text} href={value.href}/>);
+        }
+        this.state.links.forEach(getLinkDoms);
+        return navLinkDoms;
+    }
+
+    render() {
+        return (
+            <ul className="navbar-nav">
+                {this.getLinks()}
+            </ul>
+        );
+    }
 }
 
 export default NavLinkBar;
