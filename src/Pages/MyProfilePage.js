46a8ab5233e7997a56b878374c7d0ec467d0987e
benguericke <ben.guericke@gmail.com>
2022-04-20 13:18:28 -0600

    Added functionality to edit profile and delete account

--- src/Pages/MyProfilePage.js
+++ src/Pages/MyProfilePage.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Navigate } from 'react-router-dom';
 import {userContext} from "../Contexts/userContext";
+import EditProfileForm from "../Components/Login/Forms/EditProfileForm";
 
 function MyProfilePage() {
     const user = React.useContext(userContext);
+    console.log(user)
     if (user) {
         return (
             <main>
-                <div className="container">
-                    <p>{user.username}</p>
+                <div className="container pt-3">
+                    <h1> Hi {user.attributes.given_name}, </h1>
+                    <EditProfileForm/>
                 </div>
             </main>
         );
+    } else if (user === undefined) {
+        return (null);
     } else {
         return (<Navigate to={"/signIn"} replace={true}/>);
     }
