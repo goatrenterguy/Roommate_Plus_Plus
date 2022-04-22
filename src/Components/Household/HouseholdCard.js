33ae30cfa0a7cbf6cc6ff4aff8c7ed8c143b2e23
benguericke <ben.guericke@gmail.com>
2022-04-22 16:27:58 -0600

    Updated how householdCard got data will likely revert to reduce payload sizes

--- src/Components/Household/HouseholdCard.js
+++ src/Components/Household/HouseholdCard.js
@@ -1,23 +1,23 @@
-import {useEffect, useState} from "react";
-import {API, graphqlOperation} from "aws-amplify";
-import {getHousehold} from "../../graphql/queries";
+import {useState} from "react";
 
 function HouseholdCard({household}) {
-    const [householdMembers, setHouseholdMembers] = useState([]);
+    const [householdMembers] = useState(household.householdMembers.items);
+    const [householdItems] = useState(household.householdItems.items);
 
-    useEffect(() => {
-        fetchHouseholdMembers();
-    }, []);
+    // useEffect(() => {
+    //     fetchHouseholdMembers();
+    // }, []);
+    //
+    // async function fetchHouseholdMembers() {
+    //     try {
+    //         const householdMembersData = await API.graphql(graphqlOperation(getHousehold.members));
+    //         console.log(householdMembersData)
+    //         setHouseholdMembers(householdMembersData.data.listHouseholds.items);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
-    async function fetchHouseholdMembers() {
-        try {
-            const householdMembersData = await API.graphql(graphqlOperation(getHousehold.members));
-            console.log(householdMembersData)
-            setHouseholdMembers(householdMembersData.data.listHouseholds.items);
-        } catch (error) {
-            console.log(error);
-        }
-    }
     return (
         <div className="card p-3">
             <div className="card-body">
@@ -25,7 +25,14 @@ function HouseholdCard({household}) {
                 <ul>
                     {householdMembers.map((member, index) => {
                         return (
-                            <li key={"member" + index} >{member.userId}</li>
+                            <li key={"member" + index} >{member.name}</li>
+                        );
+                    })}
+                </ul>
+                <ul>
+                    {householdItems.map((item, index) => {
+                        return (
+                            <li key={"item" + index} >{item.name}</li>
                         );
                     })}
                 </ul>
