8077aa5abc8cab7fd035fe8453ebab56987a61f7
benguericke <ben.guericke@gmail.com>
2022-04-22 16:26:23 -0600

    Added route to HouseholdPage

--- src/Components/DataObjects/getNavLinkData.js
+++ src/Components/DataObjects/getNavLinkData.js
@@ -1,10 +1,12 @@
 import MyProfilePage from "../../Pages/MyProfilePage";
+import HouseholdPage from "../../Pages/HouseholdPage";
 
 function getNavLinkData() {
     return [
         {text: "Page 1", href: "/page_1", element: <p>Page 1</p>},
         {text: "Page 2", href: "/page_2", element: <p>Page 2</p>},
-        {text: "My Profile", href: "/profile", element: <MyProfilePage/>}
+        {text: "My Profile", href: "/profile", element: <MyProfilePage/>},
+        {text: "Household", href: "/household", element: <HouseholdPage/>}
     ];
 }
 
